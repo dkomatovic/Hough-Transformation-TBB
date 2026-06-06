dcb95770fbbe1863a00f0cf20d22fb596dc15995
dkomatovic <danicakomatovic@gmail.com>
2026-06-06 21:03:02 +0200

    Initialize Parallel project

---
+++ Hough Transformation/Parallel/Parallel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <filesystem>
+#include "ImageIO.h"
+#include "LineVisualization.h"
+#include "Report.h"
+#include <tbb/tbb.h>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
